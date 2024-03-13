301e2eade5765d876d3abb0118a8393f0ffaef06
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2024-03-13 21:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+class OutOfRangeError extends Error{
+	constructor(message) {
+    super(message);
+    this.name = "OutOfRangeError";
+  }
+}
+
+class InvalidExprError extends Error{
+	constructor(message) {
+    super(message);
+    this.name = "InvalidExprError";
+  }
+}
+
+function evalString(){
+	const string = document.getElementById("text").value;
+	if (string[0] == "+" || string[0] == "/" || string[0] == "*" string[0] == "-") {
+		throw new 
+	}
+}
+
+ const button = document.getElementById("btn");
+button.addEventListener("click",evalString);
